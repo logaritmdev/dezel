f650db087ff98ad67d5019471cb2b313319ca2b2
Jean-Philippe Dery <jeanphilippe.dery@gmail.com>
2019-04-01 15:57:24 -0400

    Fixed missing touchesCancelled event

--- platforms/ios/Dezel/Dezel/View/ContentWebView.swift
+++ platforms/ios/Dezel/Dezel/View/ContentWebView.swift
@@ -301,20 +301,47 @@ public class ContentWebView: WKWebView, WKNavigationDelegate, UIScrollViewDelega
 	 */
 	private var timerScrollTop: CGFloat = 0
 
+	/**
+	 * @property touchCancelDetector
+	 * @since 0.6.0
+	 * @hidden
+	 */
+	private var touchCancelDetector: TouchCancelDetector!
+
+	/**
+	 * @property application
+	 * @since 0.6.0
+	 * @hidden
+	 */
+	private var application: DezelApplicationController
+
 	//----------------------------------------------------------------------
 	// MARK: Methods
 	//----------------------------------------------------------------------
 
 	/**
 	 * @constructor
-	 * @since 0.2.0
+	 * @since 0.6.0
 	 * @hidden
 	 */
-	public convenience init(frame: CGRect, delegate: ContentWebViewDelegate?) {
+	required public init?(coder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
 
-		self.init(frame: frame, configuration: WKWebViewConfiguration())
+	/**
+	 * @constructor
+	 * @since 0.6.0
+	 * @hidden
+	 */
+	public required init(application: DezelApplicationController,frame: CGRect, delegate: ContentWebViewDelegate?) {
+
+		self.application = application
+
+		super.init(frame: frame, configuration: WKWebViewConfiguration())
 
 		self.navigationDelegate = self
+		self.contentViewDelegate = delegate
+		self.translatesAutoresizingMaskIntoConstraints = false
 
 		self.scrollView.isScrollEnabled = false
 		self.scrollView.delaysContentTouches = false
@@ -322,13 +349,11 @@ public class ContentWebView: WKWebView, WKNavigationDelegate, UIScrollViewDelega
 		self.scrollView.showsHorizontalScrollIndicator = false
 		self.scrollView.delegate = self
 
-		self.translatesAutoresizingMaskIntoConstraints = false
-
 		self.addObserver(self, forKeyPath: "loading", options: .new, context: nil)
 		self.scrollView.addObserver(self, forKeyPath: "contentSize", options: .new, context: nil)
 		self.scrollView.addObserver(self, forKeyPath: "contentOffset", options: .new, context: nil)
 
-		self.contentViewDelegate = delegate
+		self.touchCancelDetector = TouchCancelDetector(application: application, scrollView: self.scrollView)
 	}
 
 	/**
@@ -699,6 +724,136 @@ public class ContentWebView: WKWebView, WKNavigationDelegate, UIScrollViewDelega
 
 		self.timerScrollTop = t
 		self.timerScrollLeft = l
+	}
+
+	//--------------------------------------------------------------------------
+	// MARK: Classes
+	//--------------------------------------------------------------------------
+
+	/**
+	 * @class TouchCancelDetector
+	 * @since 0.6.0
+	 * @hidden
+	 */
+	private class TouchCancelDetector: UIGestureRecognizer, UIGestureRecognizerDelegate {
+
+		/**
+		 * @property application
+		 * @since 0.6.0
+		 * @hidden
+		 */
+		public weak var application: DezelApplicationController?
+
+		/**
+		 * @property touches
+		 * @since 0.6.0
+		 * @hidden
+		 */
+		private var touches: Set<UITouch> = Set()
+
+		/**
+		 * @property started
+		 * @since 0.6.0
+		 * @hidden
+		 */
+		private var started: Bool = false
+
+		/**
+		 * @property canceled
+		 * @since 0.6.0
+		 * @hidden
+		 */
+		private var canceled: Bool = false
+
+		/**
+		 * @constructor
+		 * @since 0.6.0
+		 */
+		public required init(application: DezelApplicationController, scrollView: UIScrollView) {
+
+			super.init(target: nil, action: nil)
+
+			self.delegate = self
+			self.application = application
+
+			scrollView.panGestureRecognizer.addTarget(self, action: #selector(scrollViewDidPan))
+			scrollView.addGestureRecognizer(self)
+		}
+
+		/**
+		 * @inherited
+		 * @method gestureRecognizerShouldRecognizeSimultaneouslyWith
+		 * @since 0.6.0
+		 */
+		public func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+			return true
+		}
+
+		/**
+		 * @inherited
+		 * @method touchesBegan
+		 * @since 0.6.0
+		 */
+		override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent) {
+			for touch in touches {
+				self.touches.insert(touch)
+			}
+		}
+
+		/**
+		 * @inherited
+		 * @method touchesEnded
+		 * @since 0.6.0
+		 */
+		override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent) {
+
+			for touch in touches {
+				self.touches.remove(touch)
+			}
+
+			if (self.touches.count == 0) {
+				self.restart()
+			}
+		}
+
+		/**
+		 * @inherited
+		 * @method touchesCancelled
+		 * @since 0.6.0
+		 */
+		override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent) {
+
+			for touch in touches {
+				self.touches.remove(touch)
+			}
+
+			if (self.touches.count == 0) {
+				self.restart()
+			}
+		}
+
+		/**
+		 * @method scrollViewDidPan
+		 * @since 0.6.0
+		 * @hidden
+		 */
+		@objc private func scrollViewDidPan(gesture: UIGestureRecognizer) {
+			if (self.canceled == false) {
+				self.canceled = true
+				self.application?.dispatchTouchCancel(self.touches)
+			}
+		}
+
+		/**
+		 * @method restart
+		 * @since 0.6.0
+		 * @hidden
+		 */
+		private func restart() {
+			DispatchQueue.main.asyncAfter(deadline: .now() + 0.05) {
+				self.canceled = false
+			}
+		}
 	}
 }
 
