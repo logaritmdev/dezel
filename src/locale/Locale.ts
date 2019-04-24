ba09358d9bb191d41df2b7a76bd12cd0f6666c74
Jean-Philippe Dery <jeanphilippe.dery@gmail.com>
2019-04-24 17:59:59 -0400

    Removed code that shouldn't have been there

--- src/locale/Locale.ts
+++ src/locale/Locale.ts
@@ -36,47 +36,6 @@ export class Locale {
 	@native public direction!: 'ltr' | 'rtl'
 
 	//--------------------------------------------------------------------------
-	// Methods
-	//--------------------------------------------------------------------------
-
-	/**
-	 * Stores a value synchronously.
-	 * @method set
-	 * @since 0.4.0
-	 */
-	public set(key: string, value: any) {
-		this.native.set(key, JSON.stringify(value))
-		return this
-	}
-
-	/**
-	 * Retrieves a value synchronously.
-	 * @method get
-	 * @since 0.4.0
-	 */
-	public get(key: string) {
-
-		let res = this.native.get(key)
-		if (res == null ||
-			res == 'null' ||
-			res == 'undefined') {
-			return null
-		}
-
-		return JSON.parse(res) || undefined
-	}
-
-	/**
-	 * Removes a value synchronously.
-	 * @method remove
-	 * @since 0.4.0
-	 */
-	public remove(key: string) {
-		this.native.remove(key)
-		return this
-	}
-
-	//--------------------------------------------------------------------------
 	// Native API
 	//--------------------------------------------------------------------------
 
