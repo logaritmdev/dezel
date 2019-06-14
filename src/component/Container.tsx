c2df2d17490ac4d30859df9ad59b27f40e799352
Jean-Philippe Dery <jeanphilippe.dery@gmail.com>
2019-06-14 14:50:15 -0400

    Added a container component

---
+++ src/component/Container.tsx
@@ -0,0 +1,14 @@
+import { View } from '../view/View'
+import './Container.ds'
+import './Container.ds.android'
+import './Container.ds.ios'
+
+/**
+ * TODO
+ * @class Container
+ * @super Component
+ * @since 0.7.0
+ */
+export class Container extends View {
+
+}
