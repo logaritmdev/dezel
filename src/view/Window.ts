64ff8d404e18c2b42640dd6082acbbae9204b175
Jean-Philippe Dery <jeanphilippe.dery@gmail.com>
2019-06-17 08:54:44 -0400

    Removed style sheet

--- src/view/Window.ts
+++ src/view/Window.ts
@@ -1,1 +1,32 @@
-export { Window } from './View'
+import { bridge } from '../decorator/bridge'
+import { View } from './View'
+
+@bridge('dezel.view.Window')
+
+/**
+ * Displays the top most view at the root of the hierarchy.
+ * @class Window
+ * @super View
+ * @since 0.1.0
+ */
+export class Window extends View {
+
+	/**
+	 * Initializes the window.
+	 * @constructor
+	 * @since 0.1.0
+	 */
+	constructor() {
+		super()
+		this.id = 'window'
+	}
+
+	/**
+	 * Finds a view from this window's hierarchy at the specified point.
+	 * @method viewFromPoint
+	 * @since 0.2.0
+	 */
+	public viewFromPoint(x: number, y: number, visible: boolean = true, touchable: boolean = true): View | null | undefined {
+		return this.native.viewFromPoint(x, y, visible, touchable)
+	}
+}
