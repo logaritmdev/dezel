ee57c039800abf5d98ae89ca66f80e343949590b
Jean-Philippe Dery <jeanphilippe.dery@gmail.com>
2019-12-19 11:36:00 -0500

    Added stub back

--- src/global/XMLHttpRequestUpload.ts
+++ src/global/XMLHttpRequestUpload.ts
@@ -3,12 +3,11 @@ import { EventTarget } from './EventTarget'
 
 // @bridge('dezel.global.XMLHttpRequestUpload')
 
-// /**
-//  * TODO
-//  * @class XMLHttpRequestUpload
-//  * @super EventTarget
-//  * @since 0.7.0
-//  */
-// export class XMLHttpRequestUpload extends EventTarget {
+/**
+ * @class XMLHttpRequestUpload
+ * @super EventTarget
+ * @since 0.7.0
+ */
+export class XMLHttpRequestUpload extends EventTarget {
 
-// }
+}
